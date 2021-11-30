46aff0311b75909487176bfbf76f3022099440fc
Wojciech Fiołka <56364007+Kabanosk@users.noreply.github.com>
2021-11-30 02:09:33 +0100

    tokenize data, updating data in json file

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pprint
 from random import randint
 
 import numpy as np
@@ -6,6 +7,7 @@ import pandas as pd
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation
 from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.preprocessing.text import Tokenizer
 
 import json
 
@@ -15,3 +17,10 @@ df = pd.json_normalize(data["intents"])
 
 X = np.array([df['tag'], df['patterns']])
 y = np.array([df['tag'], df['responses']])
+
+tokenizer = Tokenizer(num_words=100)
+
+
+for i in range(len(X[1])):
+    tokenizer.fit_on_texts(X[1][i])
+    word_index = tokenizer.word_index
