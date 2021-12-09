d846c9cd4b05ea51c1beabef6ee12408f1b5ee3a
Wojciech Fiołka <fiolkawojciech@gmail.com>
2021-12-09 15:24:08 +0100

    Loding data from guestions.json, start trying to make routes change after click the next button

--- web/web_page.py
+++ web/web_page.py
@@ -1,4 +1,21 @@
-from flask import Flask
+from flask import Flask, request, render_template
+import json
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='template')
 
+with open('questions.json') as file:
+    questions = json.load(file)
+answers = []
+
+
+for i in questions.keys():
+    @app.route(f'/{i}', methods=["GET", "POST"])
+    def form():
+        if request.method == "POST":
+            answers.append(request.form.get("question"))
+            return render_template("question.html",
+                                   question=questions[i]["question"],
+                                   hint=questions[i]["hint"],
+                                   next="Next")
+app.run()
+print(answers)
